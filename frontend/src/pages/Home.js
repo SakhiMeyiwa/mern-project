498ac9f17eb646fc9e6c7c920da231ab3eafd767
Sakhi Meyiwa <159584750+SakhiMeyiwa@users.noreply.github.com>
2025-01-14 13:21:53 +0200

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
   console.log('user',user)
   const fetchUserDetails = async()=>{
     try {
-        const URL = `${process.env.REACT_APP_BACKEND_URL}/api/user-details`
+        const URL = `${https://mern-project-server-theta.vercel.app}/api/user-details`
         const response = await axios({
           url : URL,
           withCredentials : true
