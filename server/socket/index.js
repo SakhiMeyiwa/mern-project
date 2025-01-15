94dc60466925f09ff29002c31e55af95aef872cd
Sakhi Meyiwa <159584750+SakhiMeyiwa@users.noreply.github.com>
2025-01-15 06:59:53 +0200

    Update index.js

--- server/socket/index.js
+++ server/socket/index.js
@@ -12,7 +12,7 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server,{
     cors : {
-        origin : https://mern-project-chat-app-sakhi.vercel.app,
+        origin : process.env.FRONTEND_URL,
         credentials : true
     }
 })
